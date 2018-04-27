a34c7a139b94f43d014eac27e9408f108b6d22fe
Hari Pulugurta <charanp@gmail.com>
2018-04-27 09:31:21 -0400

    minor changes

--- html_tree/htmltree.py
+++ html_tree/htmltree.py
@@ -2,40 +2,44 @@ import requests
 from html.parser import HTMLParser
 from queue  import Queue
 import uuid
-from collections import OrderedDict
+from stack import Stack
 
 class HtmlTagInfo:
-    def __init__(self,tag,tag_text,tag_val):
+    def __init__(self,id,tag,tag_text,tag_val,attrs):
+        self.id=id
         self.tag=tag
         self.tag_text=tag_text
         self.tag_val=tag_val
+        self.attrs=attrs
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def __init__(self,elements):
         HTMLParser.__init__(self)
         self.elements=elements
-
+        
     def handle_starttag(self, tag, attrs):
-        id=self.get_id()
-        self.elements[id]=HtmlTagInfo("<",tag,None)
+        #id=self.get_id()
+        #self.elements[id]=HtmlTagInfo("<",tag,None)
+        self.elements.append("<" + tag)
 
     def handle_endtag(self, tag):
-        id=self.get_id()
-        self.elements[id]=HtmlTagInfo(">",tag,None)
+        #id=self.get_id()
+        #self.elements[id]=HtmlTagInfo(">",tag,None)
+        self.elements.append(">" + tag)
 
     def handle_data(self, data):
-        id=self.get_id()
-        self.elements[id]=HtmlTagInfo(None,None,data)
+        #id=self.get_id()
+        #self.elements[id]=HtmlTagInfo(None,None,data)
+        self.elements.append(data)
 
     def get_id(self):
         return str(uuid.uuid4())
 
 
 dom=requests.get("https://www.google.com/")
-mappings = OrderedDict()
+mappings = []
 parser = MyHTMLParser(mappings)
 parser.feed(dom.text)
 
-elements=parser.elements
-print(elements)
+print(mappings)
