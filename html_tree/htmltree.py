fbe95980a9b582d94216cc79d1161f59925c6c3b
Hari Pulugurta <charanp@gmail.com>
2018-05-02 02:14:15 -0400

    WIP - Merge the tags

--- html_tree/htmltree.py
+++ html_tree/htmltree.py
@@ -3,6 +3,7 @@ from html.parser import HTMLParser
 from queue  import Queue
 import uuid
 from stack import Stack
+from collections import OrderedDict
 
 class HtmlTagInfo:
     def __init__(self,id,tag,tag_text,tag_val,attrs):
@@ -14,32 +15,67 @@ class HtmlTagInfo:
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
-    def __init__(self,elements):
+    def __init__(self,elements,node_id):
         HTMLParser.__init__(self)
         self.elements=elements
+        self.node_id=node_id 
         
     def handle_starttag(self, tag, attrs):
-        #id=self.get_id()
-        #self.elements[id]=HtmlTagInfo("<",tag,None)
-        self.elements.append("<" + tag)
+        key=f'< {str(self.node_id)} {tag}'
+        self.elements.append( HtmlNodeInfo(key,self.node_id+1,None))
+        self.node_id=self.node_id+1
 
     def handle_endtag(self, tag):
-        #id=self.get_id()
-        #self.elements[id]=HtmlTagInfo(">",tag,None)
-        self.elements.append(">" + tag)
+        key=f'> {str(self.node_id)} {tag}'
+        self.elements.append( HtmlNodeInfo(key,self.node_id+1,None))
+        self.node_id=self.node_id+1
 
     def handle_data(self, data):
-        #id=self.get_id()
-        #self.elements[id]=HtmlTagInfo(None,None,data)
-        self.elements.append(data)
+        self.elements.append( HtmlNodeInfo(None,self.node_id+1,None))
+        self.node_id=self.node_id+1
 
-    def get_id(self):
-        return str(uuid.uuid4())
+    def get_elements(self):
+        return self.elements
 
 
 dom=requests.get("https://www.google.com/")
+# mappings = OrderedDict()
 mappings = []
-parser = MyHTMLParser(mappings)
+parser = MyHTMLParser(mappings,0)
 parser.feed(dom.text)
 
-print(mappings)
+elements=parser.get_elements()
+# print(elements)
+
+
+stack=Stack()
+for elem in elements:
+    if elem.startswith("<"):
+        stack.push(elem)
+    
+    if elem.startswith(">"):
+        # Closing tag
+        # Pop the first item from stack
+        pop_elem=stack.pop()
+
+        # Get the data
+        pop_elem_data=stack.peek
+        pop_elem_data_key=f'{pop_elem_key}_data'
+        pop_elem_start_Key=f'{pop_elem'
+
+        print(pop_elem)
+        print(elem)
+        break
+
+
+class HtmlNodeInfo:
+    def __init__(self,key,index,data):
+        self.key=key
+        self.index=index
+        self.data=data
+
+class HtmlNode:
+    def __init__(self,start,end,data):
+        self.start=start
+        self.end=end
+        self.data=data
