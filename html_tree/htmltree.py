1fa79c291fe058abbe1cc2922707b422501babff
Hari Pulugurta <charanp@gmail.com>
2018-05-02 02:35:56 -0400

    Group tags along with their data

--- html_tree/htmltree.py
+++ html_tree/htmltree.py
@@ -5,13 +5,18 @@ import uuid
 from stack import Stack
 from collections import OrderedDict
 
-class HtmlTagInfo:
-    def __init__(self,id,tag,tag_text,tag_val,attrs):
+class HtmlNodeInfo:
+    def __init__(self,key,index,data):
+        self.key=key
+        self.index=index
+        self.data=data
+
+class HtmlNode:
+    def __init__(self,start,data,id,parent_id):
+        self.start=start
+        self.data=data        
         self.id=id
-        self.tag=tag
-        self.tag_text=tag_text
-        self.tag_val=tag_val
-        self.attrs=attrs
+        self.parent_id=parent_id
 
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
@@ -21,17 +26,17 @@ class MyHTMLParser(HTMLParser):
         self.node_id=node_id 
         
     def handle_starttag(self, tag, attrs):
-        key=f'< {str(self.node_id)} {tag}'
+        key=f'<{tag}'
         self.elements.append( HtmlNodeInfo(key,self.node_id+1,None))
         self.node_id=self.node_id+1
 
     def handle_endtag(self, tag):
-        key=f'> {str(self.node_id)} {tag}'
+        key=">"
         self.elements.append( HtmlNodeInfo(key,self.node_id+1,None))
         self.node_id=self.node_id+1
 
     def handle_data(self, data):
-        self.elements.append( HtmlNodeInfo(None,self.node_id+1,None))
+        self.elements.append( HtmlNodeInfo("",self.node_id+1,data))
         self.node_id=self.node_id+1
 
     def get_elements(self):
@@ -47,35 +52,28 @@ parser.feed(dom.text)
 elements=parser.get_elements()
 # print(elements)
 
-
+final_list=[]
 stack=Stack()
 for elem in elements:
-    if elem.startswith("<"):
+    if elem.key.startswith("<"):
         stack.push(elem)
     
-    if elem.startswith(">"):
+    if elem.key.startswith(">"):
         # Closing tag
         # Pop the first item from stack
         pop_elem=stack.pop()
 
         # Get the data
-        pop_elem_data=stack.peek
-        pop_elem_data_key=f'{pop_elem_key}_data'
-        pop_elem_start_Key=f'{pop_elem'
+        data_index=elem.index-2
+        data_node=elements[data_index]
 
-        print(pop_elem)
-        print(elem)
-        break
+        # Get the parent id
+        parent_index=pop_elem.index-2
+
+        key=f'{pop_elem.key}{elem.key}'
+        node=HtmlNode(key,data_node.data,pop_elem.index,parent_index)  
+        final_list.append(node)
+
+print(final_list)
 
 
-class HtmlNodeInfo:
-    def __init__(self,key,index,data):
-        self.key=key
-        self.index=index
-        self.data=data
-
-class HtmlNode:
-    def __init__(self,start,end,data):
-        self.start=start
-        self.end=end
-        self.data=data
