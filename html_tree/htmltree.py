cc9c92f678fd245fea13c3ebcd14e0209eb114f7
Hari Pulugurta <hari.pulugurta@tntp.org>
2018-05-04 14:58:55 -0400

    WIP:Assign correct parent ids

--- html_tree/htmltree.py
+++ html_tree/htmltree.py
@@ -5,6 +5,7 @@ import uuid
 from stack import Stack
 from collections import OrderedDict
 from pprint import pprint
+from tree_ds import Tree,NodeInfo,NodeType
 
 class HtmlNodeInfo:
     def __init__(self,key,index,data):
@@ -29,15 +30,28 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         key=f'<{tag}'
         self.elements.append( HtmlNodeInfo(key,self.node_id,None))
+        print("Encountered a start tag:" + tag + "," + str(self.node_id))
         self.node_id=self.node_id+1
 
+        if (tag.lower()=="link" or tag.lower()=="meta"):
+            #There is no end, so lets add one
+            key=f'>{tag}'
+            self.elements.append( HtmlNodeInfo(key,self.node_id,None))
+            print("Encountered a end tag:" + tag + "," + str(self.node_id))
+            self.node_id=self.node_id+1        
+
     def handle_endtag(self, tag):
-        key=">"
-        self.elements.append( HtmlNodeInfo(key,self.node_id,None))
+        key=f'>{tag}'
+        self.elements.append( HtmlNodeInfo(key,self.node_id,None))       
+        print("Encountered a end tag:" + tag + "," + str(self.node_id))
+
         self.node_id=self.node_id+1
 
     def handle_data(self, data):
         self.elements.append( HtmlNodeInfo("",self.node_id,data))
+
+        print("Encountered data:" + str(self.node_id) + "," + data)
+        
         self.node_id=self.node_id+1
 
     def get_elements(self):
@@ -45,17 +59,17 @@ class MyHTMLParser(HTMLParser):
 
 
 dom=requests.get("https://www.google.com/")
-# mappings = OrderedDict()
 mappings = []
 parser = MyHTMLParser(mappings,0)
 
 dom_text=dom.text
 
-dom_text="<div>test<br/><p>ptest</p></div>"
+# dom_text="<body>test<br/><p>ptest</p></body>"
 
 parser.feed(dom_text)
 
 elements=parser.get_elements()
+
 
 final_list=[]
 stack=Stack()
@@ -77,20 +91,15 @@ for elem in elements:
             data_node=[dn for dn in datas_nodes if dn.data is not None]
         data=None if len(data_node)==0 else data_node[0]
 
-        # Get the parent id
-        parent_index=pop_elem.index-1
-        parents= elements[parent_index::-1]
-        parent_elem_index=-1
-        for parent in parents:
-            if parent.data is None:
-                parent_elem_index=parent.index
-                break
+        if stack.is_empty():
+            parent_elem_index=pop_elem.index 
+        else:
+            parent_elem_index=stack.peek().index  
         
-        parent_elem_index= parent_elem_index if parent_elem_index>=0 else pop_elem.index
-
         key=f'{pop_elem.key}{elem.key}'
         node=HtmlNode(key,data,pop_elem.index,parent_elem_index)  
         final_list.append(node)
+ 
 
 final_list=sorted(final_list,key=lambda p: (p.parent_id,p.id))
 pprint( "item_id,parent_id,item" )
@@ -98,3 +107,27 @@ for item in final_list:
     pprint( str(item.id) + "," + str(item.parent_id) + "," + item.start )
 
 
+# parents=sorted(list(set([ p.parent_id for p in final_list  ])))
+# print(parents) 
+
+# childs=sorted(list(set([ p.id for p in final_list  ])))
+# print(childs) 
+
+
+# t=Tree()
+# n=NodeInfo(final_list[0].start,None,NodeType.ROOT) 
+# t.add_node(n,None)
+
+# for item in final_list[1:]:
+#     # Get this items parent
+#     parent=final_list[item.parent_id]
+#     parent_node= t.get_node(parent.start)
+
+#     # Create child node
+#     cn=NodeInfo(item.start,None,None) 
+
+#     # Add to tree
+#     t.add_node(cn,parent_node)
+
+# t.plot_tree()
+# # dom_text="<div>test<br/><p>ptest</p></div>"
